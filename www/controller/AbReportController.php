516b2bf724c754b81d4932629b05ad27b3c25164
healer <healer.kx.yu@gmail.com>
2016-03-29 13:46:00 +0800

    AbReport base code

--- www/controller/AbReportController.php
+++ www/controller/AbReportController.php
@@ -7,8 +7,24 @@
  */
 class AbReportController extends AbBaseController
 {
-    public function indexAction()
+    public function createAction()
     {
+        $tableNames = $this->tableNames();
+        $data = array('table_names' => $tableNames);
 
+        $views = [
+            ["name" =>'新建报表', "template"=> "abreport/create"] ];
+
+        parent::addDialog('添加列', 'abreport/dialog-new-row');
+        parent::showTabViews($views, '创建报表', $data);
+    }
+
+    private function tableNames() {
+        $a = $this->db->fetchAll("SHOW tables");
+        $tableNames = array();
+        foreach ($a as $table) {
+            array_push($tableNames, $table['Tables_in_badmin']);
+        }
+        return $tableNames;
     }
 }
